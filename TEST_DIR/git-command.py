16011d70723c6ef3f1c7eb0b080253f44beab5a6
kasper <awecefil@gmail.com>
2023-05-28 11:19:06 +0800

    record git command

---
+++ TEST_DIR/git-command.py
@@ -0,0 +1,15 @@
+# TODO 紀錄指令
+
+# git clone https://github.com/用戶名稱/雲端儲存庫名稱 - 在本地端建立一個儲存庫，儲存庫的內容由雲端儲存庫複製而來，兩者建立連結
+# git init - 直接在本機端建立儲存庫做版控，之後可透過其他方式上傳到 Github
+
+
+# git statul - 查看狀態
+
+# git add 資料夾路徑 - 請 Git 追蹤此資料夾，此後檔案的狀態將存入 staging area 整備區
+
+# [Note] 一定要 add + commit 兩步驟才是正式紀錄到儲存庫，add 只是暫存
+# git commit - 送出 commit 即是將當前資料夾內容創造出一個checkpoint，也等於一個版本
+    # -m "此 commit 的說明訊息"
+
+# git push - 將本機儲存庫的改變同步到雲端儲存庫
