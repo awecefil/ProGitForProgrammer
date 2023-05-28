22bd6ae51294ad985ab85ae6785373325ffd5981
Kasper Lin <awecefil@gmail.com>
2023-05-28 21:18:23 +0800

    check argument type before operation in add()

--- TEST_DIR/calculator.py
+++ TEST_DIR/calculator.py
@@ -1,7 +1,12 @@
 import numpy as np
 
 def add(n1, n2):
-    return n1 + n2
+    if isinstance(n1, str):
+        print("n1 should not be a string")
+    elif isinstance(n2, str):
+        print("n2 should not be a string")
+    else:
+        return n1 + n2
 
 def minus(n1, n2):
     return n1 - n2
@@ -15,3 +20,4 @@ def division(n1, n2):
         return n1/n2
     else:
         print("n2 should not be zero")
+
