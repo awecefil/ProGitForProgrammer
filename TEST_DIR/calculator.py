2217482eec48f907abf27dbf2e674194d5b9b6b8
Kasper Lin <awecefil@gmail.com>
2023-05-28 20:59:15 +0800

    add division() in calculator.py

--- TEST_DIR/calculator.py
+++ TEST_DIR/calculator.py
@@ -8,3 +8,10 @@ def minus(n1, n2):
 
 def multiply(n1, n2):
     return n1 * n2
+
+def division(n1, n2):
+    
+    if n2 != 0:
+        return n1/n2
+    else:
+        print("n2 should not be zero")
